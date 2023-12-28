90335824689890c2b34d89c4856b242b5adc81c9
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-12-28 23:40:54 +0100

    feat: Initial commit

---
+++ src/DailyTransaction.type.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod';
+import {ZDate} from './Base.type';
+
+export enum EDailyTransactionType {
+  INCOME = 'INCOME',
+  SPENDINGS = 'SPENDINGS',
+  BALANCE = 'BALANCE',
+}
+
+export const ZDailyTransaction = z.object({
+  date: ZDate,
+  amount: z.number(),
+});
+export type TDailyTransaction = z.infer<typeof ZDailyTransaction>;
