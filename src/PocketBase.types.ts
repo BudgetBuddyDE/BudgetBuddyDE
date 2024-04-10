e989be504e77d61cd5dc1ca04941bdc099df2a12
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-04-10 20:28:08 +0200

    fix: Add missing collection of tables

--- src/PocketBase.types.ts
+++ src/PocketBase.types.ts
@@ -1,6 +1,19 @@
 import {z} from 'zod';
 import {ZDate} from './Base.type';
 
+export enum PocketBaseCollection {
+  USERS = 'users',
+  BUDGET = 'budgets',
+  CATEGORY = 'categories',
+  PAYMENT_METHOD = 'payment_methods',
+  STOCK_EXCHANGE = 'stock_exchanges',
+  STOCK_POSITION = 'stock_positions',
+  STOCK_WATCHLIST = 'stock_watchlists',
+  SUBSCRIPTION = 'subscriptions',
+  TRANSACTION = 'transactions',
+  V_MONTHLY_BALANCES = 'v_monthly_balances',
+}
+
 /**
  * @description	15 characters string to store as record ID. If not set, it will be auto generated.
  */
