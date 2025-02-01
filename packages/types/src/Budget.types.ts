77b5c1a7f73b55ea903b9be059b2a37d65e2e69c
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-02-01 20:10:47 +0100

    refactor: Update budget related schemas and types

--- packages/types/src/Budget.types.ts
+++ packages/types/src/Budget.types.ts
@@ -6,25 +6,60 @@ export const ZBudget = z.object({
   ...ZBaseModel.shape,
   ...z.object({
     owner: ZId,
-    category: ZId,
+    label: z.string(),
+    category: z.array(ZId),
     budget: z.number(),
-    expand: z.object({
-      category: ZCategory,
-    }),
+    type: z.enum(['include', 'exclude']),
   }).shape,
 });
 export type TBudget = z.infer<typeof ZBudget>;
 
+export const ZExpandedBudget = z.object({
+  ...ZBaseModel.shape,
+  ...z.object({
+    owner: ZId,
+    label: z.string(),
+    category: z.array(ZId),
+    expand: z.object({
+      categories: z.array(ZCategory),
+    }),
+    budget: z.number(),
+    type: z.enum(['include', 'exclude']),
+  }).shape,
+});
+export type TExpandedBudget = z.infer<typeof ZExpandedBudget>;
+
+export const ZBudgetProgress = z.object({
+  ...ZBudget.shape,
+  progress: z.number(),
+});
+export type TBudgetProgress = z.infer<typeof ZBudgetProgress>;
+
+export const ZExpandedBudgetProgress = z.object({
+  ...ZBudget.shape,
+  progress: z.number(),
+});
+export type TExpandedBudgetProgress = z.infer<typeof ZExpandedBudgetProgress>;
+
 export const ZCreateBudgetPayload = z.object({
   owner: ZId,
-  category: ZId,
-  budget: z.number(),
+  label: z.string(),
+  category: z.array(ZId),
+  budget: z.number().min(0),
+  type: z.enum(['include', 'exclude']),
 });
 export type TCreateBudgetPayload = z.infer<typeof ZCreateBudgetPayload>;
 
 export const ZUpdateBudgetPayload = z.object({
   owner: ZId,
-  category: ZId,
-  budget: z.number(),
+  label: z.string(),
+  category: z.array(ZId),
+  budget: z.number().min(0),
+  type: z.enum(['include', 'exclude']),
 });
 export type TUpdateBudgetPayload = z.infer<typeof ZUpdateBudgetPayload>;
+
+export const ZDeleteBudgetPayload = z.object({
+  id: ZId,
+});
+export type TDeleteBudgetPayload = z.infer<typeof ZDeleteBudgetPayload>;
