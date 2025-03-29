f53dcf1836259ca05aba74dd7f602379a9d70e3b
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-03-29 19:25:25 +0100

    revert: Revert rewrite
    Doesn't work correctly with dynamic links

--- apps/new-website/next.config.mjs
+++ apps/new-website/next.config.mjs
@@ -5,25 +5,25 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  rewrites() {
-    return {
-      beforeFiles: [
-        // Wenn der Host `docs.budget-buddy.de` ist,
-        // wird der `/docs`-Teil aus der URL entfernt und auf die neue URL umgeschrieben
-        {
-          source: '/docs/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'docs.budget-buddy.de',
-            },
-          ],
-          destination: 'https://docs.budget-buddy.de/:path*',
-        },
-      ],
-    };
-  },
-  assetPrefix: 'https://new.budget-buddy.de',
+  // rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       // Wenn der Host `docs.budget-buddy.de` ist,
+  //       // wird der `/docs`-Teil aus der URL entfernt und auf die neue URL umgeschrieben
+  //       {
+  //         source: '/docs/:path*',
+  //         has: [
+  //           {
+  //             type: 'host',
+  //             value: 'docs.budget-buddy.de',
+  //           },
+  //         ],
+  //         destination: 'https://docs.budget-buddy.de/:path*',
+  //       },
+  //     ],
+  //   };
+  // },
+  // assetPrefix: 'https://new.budget-buddy.de',
 };
 
 export default withMDX(config);
