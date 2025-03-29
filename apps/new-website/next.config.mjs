e58cc3023d71584c58e1fe6d78fef3a8c86f0aa1
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-03-29 18:45:57 +0100

    build: Redirect docs.*.de to documentation

--- apps/new-website/next.config.mjs
+++ apps/new-website/next.config.mjs
@@ -5,6 +5,25 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `newdocs.budget-buddy.de`,
+        // this rewrite will be applied
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'docs.budget-buddy.de',
+            },
+          ],
+          destination: '/docs/:path*',
+        },
+      ],
+    };
+  },
+  assetPrefix: 'https://new.budget-buddy.de',
 };
 
 export default withMDX(config);
