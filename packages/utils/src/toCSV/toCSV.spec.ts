06a838e4d0431eb45257b535219877a57854a498
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-06-05 16:28:08 +0200

    feat: Add toCSV function

---
+++ packages/utils/src/toCSV/toCSV.spec.ts
@@ -0,0 +1,16 @@
+import {toCSV} from './toCSV';
+
+describe('toCSV', () => {
+  test.skip('it should throw an error when an invalid input object is provided', () => {});
+
+  test('it should throw an error when no fields are provided', () => {
+    const input = [
+      {name: 'John', surname: 'Doe', age: 55, sex: 'm'},
+      {name: 'Hubert', surname: 'Doe', age: 12, sex: 'm'}
+    ];
+
+    expect(() => toCSV(input, [])).toThrowError(/no fields have been defined/);
+  });
+
+  test.skip('it should transform the input according to the provided parameter-values', () => {});
+});
