8c16fcca70714f6124050b4e9e3982d7428ce8e7
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-06-09 19:21:29 +0200

    fix(backend): Skip auth when creating a user

--- services/backend/srv/auth.ts
+++ services/backend/srv/auth.ts
@@ -1,35 +1,41 @@
 import cds from '@sap/cds';
-import {type NextFunction, type Request, type Response} from 'express';
+import { type NextFunction, type Request, type Response } from 'express';
 
-import {config} from './core/config';
-import {type TSession, type TUser} from './types';
-import {type Nullable} from './types/utility';
-import {AuthHelper} from './utils';
+import { config } from './core/config';
+import { type TSession, type TUser } from './types';
+import { type Nullable } from './types/utility';
+import { AuthHelper } from './utils';
 
-const authLogger = config.getLogger('auth', {label: 'auth'});
+const authLogger = config.getLogger('auth', { label: 'auth' });
 
-type Req = Request & {user: cds.User; tenant: string; requestId: string};
+type Req = Request & { user: cds.User; tenant: string; requestId: string };
 
 export default async function custom_auth(req: Req, _res: Response, next: NextFunction) {
   req.requestId = cds.utils.uuid();
-  let logOptions = {requestId: req.requestId, baseUrl: req.baseUrl, path: req.path, user: req.user, method: req.method};
+  let logOptions = {
+    requestId: req.requestId,
+    baseUrl: req.baseUrl,
+    path: req.path,
+    user: req.user,
+    method: req.method,
+  };
 
   // This section prevents unauthorized users from creating entries in the backend user table.
   // The user table serves as a central reference for managing relationships with other database tables.
-  // if (`${req.baseUrl}${req.path}` === '/service/user/User' && req.method === 'POST') {
-  //   authLogger.warn('Skipping authentication for internal user creation', logOptions);
-  //   // FIXME: This is a workaround to allow internal user creation
-  //   req.user = new cds.User({
-  //     id: 'internal',
-  //     roles: ['system'],
-  //     attr: {},
-  //   });
-  //   return next();
-  // }
+  if (`${req.baseUrl}${req.path}` === '/service/user/User' && req.method === 'POST') {
+    authLogger.warn('Skipping authentication for internal user creation', logOptions);
+    // FIXME: This is a workaround to allow internal user creation
+    // req.user = new cds.User({
+    //   id: 'internal',
+    //   roles: ['system'],
+    //   attr: {},
+    // });
+    return next();
+  }
 
   const authHelper = new AuthHelper();
   try {
-    let data: Nullable<{session: TSession; user: TUser}> = null;
+    let data: Nullable<{ session: TSession; user: TUser }> = null;
     try {
       const headers = new Headers();
       if (req.headers.authorization) {
@@ -42,7 +48,7 @@ export default async function custom_auth(req: Req, _res: Response, next: NextFu
         // If the request has a cookie header, we use it to get the session
         headers.set('Cookie', req.headers.cookie);
       }
-      data = await authHelper.getSession({headers: headers});
+      data = await authHelper.getSession({ headers: headers });
     } catch (error) {
       return next(error);
     }
@@ -53,7 +59,7 @@ export default async function custom_auth(req: Req, _res: Response, next: NextFu
 
     req.user = authHelper.mapToUserObj(sessionUser);
   } catch (err) {
-    authLogger.error('Unexpected error in authentication', {err}, logOptions);
+    authLogger.error('Unexpected error in authentication', { err }, logOptions);
     return next(err);
   }
 
