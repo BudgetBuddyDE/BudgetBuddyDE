09ffe161807e10c8be52c9534c3f4352de09e0dd
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-06-10 21:53:57 +0200

    fix(backend): Resolve typo in env-name

--- services/backend/srv/core/config.ts
+++ services/backend/srv/core/config.ts
@@ -1,35 +1,35 @@
-import cds from "@sap/cds";
-import winston from "winston";
-import LokiTransport from "winston-loki";
+import cds from '@sap/cds';
+import winston from 'winston';
+import LokiTransport from 'winston-loki';
+
+const { NODE_ENV, LOKI_HOST, LOG_LEVEL } = process.env;
 
 const META_INFORMATION = {
-  service: "backend",
-  version: "1.0.0",
-  environment: process.env.NODE_ENV || "development",
-  project: "budgetbuddyde",
+  service: 'backend',
+  version: '1.0.0',
+  environment: NODE_ENV || 'development',
+  project: 'budgetbuddyde',
 };
 
 const format = winston.format.combine(
   winston.format.errors({ stack: true }),
-  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
   winston.format.align(),
   winston.format.splat(),
   winston.format.printf(({ timestamp, level, message, stack }) => {
     return `${timestamp} ${level}: ${stack || message}`;
-  }),
+  })
 );
 
 cds.log.Logger = cds.log.winstonLogger({
-  level: process.env.LOG_LEVEL || "INFO",
+  level: LOG_LEVEL || 'INFO',
   format: format,
   transports: [
     new winston.transports.Console(),
-    ...(process.env.NODE_ENV === "production" &&
-    process.env.LOKI_HOST !== undefined &&
-    process.env.LOGI_HOST !== ""
+    ...(NODE_ENV === 'production' && LOKI_HOST !== undefined && LOKI_HOST !== ''
       ? [
           new LokiTransport({
-            host: process.env.LOKI_HOST,
+            host: LOKI_HOST,
             labels: META_INFORMATION,
           }),
         ]
@@ -39,9 +39,9 @@ cds.log.Logger = cds.log.winstonLogger({
 
 export const config = {
   getLogger: (
-    name: string = "bb",
+    name: string = 'bb',
     options: Parameters<typeof cds.log>[1] = {
-      label: "BudgetBuddy",
-    },
+      label: 'BudgetBuddy',
+    }
   ) => cds.log(name, options),
 };
