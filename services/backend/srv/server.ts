1b1cb5dde1bc7a55f6e3d58b022a1266d3b7e38f
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-07-28 21:19:21 +0200

    refactor(backend): Get trusted origins from environment variable

--- services/backend/srv/server.ts
+++ services/backend/srv/server.ts
@@ -1,20 +1,20 @@
-import cds from "@sap/cds";
+import 'dotenv/config';
+import cds from '@sap/cds';
 
-// FIXME: Retrieve list of allowed origins using environment variables or a configuration file
-const ORIGINS = { "http://localhost:3000": 1 };
+const ORIGINS = Object.fromEntries(
+  (process.env.ORIGINS || 'http://localhost:3000').split(',').map((origin) => [origin, 1])
+);
 
-cds.on("bootstrap", (app) => {
+cds.on('bootstrap', (app) => {
   // FIXME: This is a temporary solution, consider using a more robust CORS middleware in production
   app.use((req, res, next) => {
     if (req.headers.origin && req.headers.origin in ORIGINS) {
       res
-        .set("Access-Control-Allow-Origin", req.headers.origin)
-        .set("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE")
-        .set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        .set("Access-Control-Allow-Credentials", "true");
-      cds
-        .log("cors")
-        .debug(`CORS preflight request from ${req.headers.origin}`);
+        .set('Access-Control-Allow-Origin', req.headers.origin)
+        .set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+        .set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        .set('Access-Control-Allow-Credentials', 'true');
+      cds.log('cors').debug(`CORS preflight request from ${req.headers.origin}`);
     }
     next();
   });
