64728cd59932a56c62360debbf70d9ddc107a8c7
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-11-16 19:46:53 +0100

    feat(backend): Implement job to process recurring payments

--- services/backend/srv/server.ts
+++ services/backend/srv/server.ts
@@ -1,5 +1,12 @@
 import "dotenv/config";
 import cds from "@sap/cds";
+import cron from "node-cron";
+import {
+  Subscriptions,
+  Transaction,
+  Transactions,
+} from "#cds-models/BackendService";
+import { format } from "date-fns";
 
 const ORIGINS = Object.fromEntries(
   (process.env.ORIGINS || "http://localhost:3000")
@@ -14,7 +21,7 @@ cds.on("bootstrap", (app) => {
     if (!origin) {
       cds.log("cors").debug("No origin header present in the request");
       // REVISIT: Bad behavior? BUT it will be resolved by the next major version
-      return next()
+      return next();
     }
     cds
       .log("cors")
@@ -38,4 +45,48 @@ cds.on("bootstrap", (app) => {
       return next();
     }
   });
+
+  const jobName = "process-recurring-payments";
+  cron.schedule(
+    "30 1 * * *",
+    async () => {
+      try {
+        cds.log(jobName).info("Starting recurring payments processing job...");
+        const today = format(new Date(), "yyyy-MM-dd");
+        const records = await SELECT.from(Subscriptions).where({
+          nextExecution: today,
+        });
+
+        await INSERT.into(Transactions).entries(
+          records.map(
+            (subscription) =>
+              ({
+                ID: cds.utils.uuid(),
+                owner: subscription.owner,
+                processedAt: today,
+                transferAmount: subscription.transferAmount,
+                toCategory_ID: subscription.toCategory_ID,
+                toPaymentMethod_ID: subscription.toPaymentMethod_ID,
+                receiver: subscription.receiver,
+                information: subscription.information,
+                createdAt: new Date().toISOString(),
+                createdBy: "system",
+                modifiedAt: new Date().toISOString(),
+                modifiedBy: "system",
+              }) as Transaction,
+          ),
+        );
+
+        cds
+          .log(jobName)
+          .info(`Processed ${records.length} recurring payments.`);
+      } catch (err) {
+        cds.log(jobName).error("Error processing recurring payments:", err);
+      }
+    },
+    {
+      name: jobName,
+      timezone: "Europe/Berlin",
+    },
+  );
 });
