d846577654acd59493bb4078bc6bb1c242bc2baf
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-11-29 14:11:46 +0100

    feat(backend): Add `search` and `pagination` to `categories`

--- services/new-backend/src/router/category.router.ts
+++ services/new-backend/src/router/category.router.ts
@@ -9,25 +9,46 @@ import {ApiResponse, HTTPStatusCode} from '../models';
 
 export const categoryRouter = Router();
 
-categoryRouter.get('/', async (req, res) => {
-  const userId = req.context.user?.id;
-  if (!userId) {
-    ApiResponse.builder().withStatus(HTTPStatusCode.UNAUTHORIZED).withMessage('Unauthorized').buildAndSend(res);
-    return;
-  }
-  const records = await db.query.categories.findMany({
-    where(fields, operators) {
-      return operators.eq(fields.ownerId, userId);
-    },
-  });
+categoryRouter.get(
+  '/',
+  validateRequest({
+    query: z.object({
+      search: z.string().optional(),
+      from: z.coerce.number().optional(),
+      to: z.coerce.number().optional(),
+    }),
+  }),
+  async (req, res) => {
+    const userId = req.context.user?.id;
+    if (!userId) {
+      ApiResponse.builder().withStatus(HTTPStatusCode.UNAUTHORIZED).withMessage('Unauthorized').buildAndSend(res);
+      return;
+    }
+    const records = await db.query.categories.findMany({
+      where(fields, operators) {
+        if (req.query.search) {
+          return operators.and(
+            operators.eq(fields.ownerId, userId),
+            operators.or(
+              operators.ilike(fields.name, `%${req.query.search}%`),
+              operators.ilike(fields.description, `%${req.query.search}%`),
+            ),
+          );
+        }
+        return operators.eq(fields.ownerId, userId);
+      },
+      offset: req.query.from,
+      limit: req.query.to ? req.query.to - (req.query.from || 0) : undefined,
+    });
 
-  ApiResponse.builder<typeof records>()
-    .withStatus(HTTPStatusCode.OK)
-    .withMessage("Fetched user's categories successfully")
-    .withData(records)
-    .withFrom('db')
-    .buildAndSend(res);
-});
+    ApiResponse.builder<typeof records>()
+      .withStatus(HTTPStatusCode.OK)
+      .withMessage("Fetched user's categories successfully")
+      .withData(records)
+      .withFrom('db')
+      .buildAndSend(res);
+  },
+);
 
 categoryRouter.get(
   '/:id',
