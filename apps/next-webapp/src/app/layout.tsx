187db0cdedca573f9b7de4cbe544bfb9c8fd05ff
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-13 14:07:52 +0100

    fix(webapp): Resolve linter warning

--- apps/next-webapp/src/app/layout.tsx
+++ apps/next-webapp/src/app/layout.tsx
@@ -1,14 +1,20 @@
+import type { Metadata, Viewport } from "next";
 import type React from "react";
 import { LayoutWrapper } from "./layout-wrapper";
 import { StoreProvider } from "./StoreProvider";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Budget Buddy",
   description: "Manage your budgets effortlessly with Budget Buddy.",
-  manifest: "/manifest.json",
-  themeColor: "#151936",
+  manifest: "/manifest.json"
 };
 
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#151936' },
+    { media: '(prefers-color-scheme: dark)', color: '#151936' },
+  ],
+}
 export default function RootLayout({
 	children,
 }: {
