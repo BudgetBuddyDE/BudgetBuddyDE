48196434e51e7e95fee199341dc57345ec4ea1c7
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-13 14:07:31 +0100

    fix(webapp): Rename `providerId` to `provider`

--- apps/next-webapp/src/components/User/UserAccounts/AccountList.tsx
+++ apps/next-webapp/src/components/User/UserAccounts/AccountList.tsx
@@ -21,7 +21,7 @@ import { Formatter } from "@/utils/Formatter";
 export type AccountListProps = {
 	accounts: {
 		id: string;
-		providerId: string;
+		provider: string;
 		createdAt: Date;
 		updatedAt: Date;
 		accountId: string;
@@ -70,7 +70,7 @@ export const AccountList: React.FC<AccountListProps> = ({ accounts }) => {
 		<List disablePadding>
 			{accounts.map((account, idx, arr) => {
 				const accountId = account.id;
-				const providerId = account.providerId;
+				const providerId = account.provider;
 				const createdAt = Formatter.date.format(account.createdAt);
 				return (
 					<React.Fragment key={account.id}>
