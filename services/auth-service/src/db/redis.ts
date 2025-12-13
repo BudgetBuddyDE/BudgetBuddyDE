b0b2eb3a1054a1a83b5979cbd2d31dcb4e7bcbc4
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-14 00:29:54 +0100

    refactor(auth-service): Add `REDIS_DB` env
    This will enable developers to use one instance and seperate data by using differnet databases

--- services/auth-service/src/db/redis.ts
+++ services/auth-service/src/db/redis.ts
@@ -9,7 +9,7 @@ let redis: Redis | null = null;
 export function getRedisClient(): Redis {
   if (!redis) {
     redis = new Redis(process.env.REDIS_URL as string, {
-      db: 0,
+      db: Number(process.env.REDIS_DB) || 0,
     });
 
     redis.on('connect', () => {
