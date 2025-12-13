d369facb3d3c8a6e78a3dcc522deece341c45b41
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-13 16:26:42 +0100

    chore(backend): Remove old backend

--- services/backend/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import cds from "@sap/cds/eslint.config.mjs";
-import cdsPlugin from "@sap/eslint-plugin-cds";
-
-export default [...cds.recommended, cdsPlugin.configs.recommended];
--- services/backend/srv/cache/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./MetalCache";
-export * from "./AssetCache";
