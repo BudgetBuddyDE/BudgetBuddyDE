19a75549b6a0118fc80574a4028fc73005b4068a
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-20 21:54:48 +0100

    fix(backend): Trim `receiver` of `transaction_receiver_view`

--- services/backend/src/db/schema/views.ts
+++ services/backend/src/db/schema/views.ts
@@ -1,17 +1,18 @@
+import {sql} from 'drizzle-orm';
 import {backendSchema} from './schema';
 import {recurringPayments, transactions} from './tables';
 
 export const transactionReceiverView = backendSchema.view('transaction_receiver_view').as(qb =>
   qb
     .selectDistinct({
-      receiver: transactions.receiver,
+      receiver: sql<string>`TRIM(${transactions.receiver})`.as('receiver'),
       ownerId: transactions.ownerId,
     })
     .from(transactions)
     .union(
       qb
         .selectDistinct({
-          receiver: recurringPayments.receiver,
+          receiver: sql<string>`TRIM(${recurringPayments.receiver})`.as('receiver'),
           ownerId: recurringPayments.ownerId,
         })
         .from(recurringPayments),
