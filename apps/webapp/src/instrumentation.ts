ec60473cf522fad85804c02f3bd7da4e0e6acfe5
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-28 12:35:45 +0100

    refactor(webapp): Only start otel instrumentation in prod

--- apps/webapp/src/instrumentation.ts
+++ apps/webapp/src/instrumentation.ts
@@ -1,8 +1,8 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== 'production') {
       console.log('Skipping instrumentation registration in non-production environment');
-      return
+      return;
     }
     await import('./instrumentation.node');
   } else console.log('Skipping instrumentation registration for non-nodejs runtime');
