51d8a0b29e3fab0dcbc3aee63adbc05480a9b3ca
Thorben <38816229+tklein1801@users.noreply.github.com>
2025-12-28 11:53:47 +0100

    fix(webapp): Only start tracing in production environment

--- apps/webapp/src/instrumentation.ts
+++ apps/webapp/src/instrumentation.ts
@@ -1,5 +1,9 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    if (process.env.NODE_ENV !== "production") {
+      console.log('Skipping instrumentation registration in non-production environment');
+      return
+    }
     await import('./instrumentation.node');
   } else console.log('Skipping instrumentation registration for non-nodejs runtime');
 }
