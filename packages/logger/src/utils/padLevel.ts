fbda37d7704b300f20ffd74355c700dbcc3b2b4b
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-01-01 17:07:00 +0100

    fix(logger): Don't transport level

--- packages/logger/src/utils/padLevel.ts
+++ packages/logger/src/utils/padLevel.ts
@@ -3,7 +3,6 @@ import { format } from "winston";
 export function padLevel(whitespaceCount: number) {
 	return format((info) => {
 		info.level = info.level.padEnd(whitespaceCount, " ");
-		info.level = info.level.toUpperCase();
 		return info;
 	})();
 }
