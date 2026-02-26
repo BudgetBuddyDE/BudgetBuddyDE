c40517418964108f413251a623c979b9e7f2d4ce
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-02-26 21:22:14 +0100

    chore(website): Delete old website

--- apps/website/src/instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    // await import('./instrumentation.node');
-    console.log('Skipping instrumentation registration for nodejs runtime');
-  } else console.log('Skipping instrumentation registration for non-nodejs runtime');
-}
