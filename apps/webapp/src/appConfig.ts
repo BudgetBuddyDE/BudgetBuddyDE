77737e0def6e55448f78de698de959abef20dbc0
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-06-21 18:31:53 +0200

    refactor(webapp): Rename config name
    And run formatter across project

--- apps/webapp/src/appConfig.ts
+++ apps/webapp/src/appConfig.ts
@@ -1,4 +1,4 @@
-export const config = {
+export const appConfig = {
   tables: {
     /**
      * Number of items to show per page in paginated lists. This setting is used across the application to ensure consistency in pagination behavior.
