2c438a5183c7291e93f21e6b278b77b553bfeb40
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-07-18 03:14:59 +0200

    refactor(webapp): show app informations

--- apps/webapp/src/app/(dashboard)/settings/api-keys/page.tsx
+++ apps/webapp/src/app/(dashboard)/settings/api-keys/page.tsx
@@ -2,12 +2,23 @@ import {Grid} from '@mui/material';
 import React from 'react';
 import {PathnameErrorBoundary} from '@/components/ErrorBoundary';
 import {CircularProgress} from '@/components/Loading';
+import {AppInformation} from '@/components/Settings/AppInformation';
 import {ApiKeyTable} from '@/components/User/ApiKey';
 
 export default function ApiKeysPage() {
   return (
     <Grid container spacing={2}>
-      <Grid size={{xs: 12, md: 7}}>
+      <Grid container size={{xs: 12, md: 3.5}} spacing={2}>
+        <Grid size={{xs: 12}}>
+          <PathnameErrorBoundary>
+            <React.Suspense fallback={<CircularProgress />}>
+              <AppInformation />
+            </React.Suspense>
+          </PathnameErrorBoundary>
+        </Grid>
+      </Grid>
+
+      <Grid size={{xs: 12, md: 8.5}}>
         <PathnameErrorBoundary>
           <React.Suspense fallback={<CircularProgress />}>
             <ApiKeyTable />
