ca26ec994b514d2805127c2cbb08811a1aafb568
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-09-12 00:47:43 +0200

    refactor(api): remove dead auth, batch and type exports

--- packages/api/src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-export type Entity = 'transaction' | 'recurringPayment' | 'budget' | 'category' | 'paymentMethod';
-
-export type EntityAction = 'create' | 'read' | 'update' | 'delete';
-
-export type EntityOperation = 'read' | 'write';
-
-export type Permission = `${Entity}:${EntityOperation}`;
-
-export type PermissionConfig = Partial<Record<Entity, EntityOperation[]>>;
