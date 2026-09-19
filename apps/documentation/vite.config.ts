30060c11928066dbf04bc7733b4471dfc530384c
Thorben <38816229+tklein1801@users.noreply.github.com>
2026-09-19 11:34:59 +0200

    refactor(documentation): change default port

--- apps/documentation/vite.config.ts
+++ apps/documentation/vite.config.ts
@@ -4,5 +4,8 @@ import press from 'fumapress/vite';
 import {fumadocsMdx} from 'fumadocs-mdx/vite';
 
 export default defineConfig({
+  server: {
+    port: 3080,
+  },
   plugins: [press(), fumadocsMdx(), tailwindcss()],
 });
